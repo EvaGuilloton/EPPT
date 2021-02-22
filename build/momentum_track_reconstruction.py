cc4118e85f39cb875682bf1495aac5888ee685cf
evguillo <eva.guilloton@cern.ch>
2021-02-22 03:32:41 +0100

    Adding of comments
    Adding of energy correction

--- build/momentum_track_reconstruction.py
+++ build/momentum_track_reconstruction.py
@@ -11,141 +11,151 @@ from scipy.optimize import curve_fit
 from numpy.polynomial import Polynomial
 
 
-# Arrays and files #
-case = input("1: mu,100GeV,0.5T; 2: mu,100GeV,0.25T; 3: mu,100GeV,1T; 4: mu,50GeV,0.5T; 5: mu,200GeV,0.5T; 6: with delta_x = 0; 7: with lead layer before magnet; 8: with lead layer after magnet")
+# Choosing the case to study #
+case = input("Choose the case to study : 1: 100GeV,0.5T; 2: 100GeV,0.25T; 3: 100GeV,1T; 4: 50GeV,0.5T; 5: 200GeV,0.5T; 6: with delta_x = 0; 7: with lead layer before magnet; 8: with lead layer after magnet   ")
 
 if case == 1:
     inf = ROOT.TFile.Open("B5_out_mu_100GeV_05T.root")
     tree = inf.Get("B5")
-    B = 0.5
-    delta_x = 1E-4
-    name = "mu_100GeV_05T"
+    B = 0.5 #Strenght of the magnetic field in Tesla
+    q = 1. #Charge of one particle
+    res_x = 1E-4 #Precision on position in x-coordinate
+    name = "mu_100GeV_05T" #label to help differenciate the ouputs
 elif case == 2:
     inf = ROOT.TFile.Open("B5_out_mu_100GeV_025T.root")
     tree = inf.Get("B5")
-    B = 0.25
-    delta_x = 1E-4
-    name = "mu_100GeV_025T"
+    B = 0.25 #Strenght of the magnetic field in Tesla
+    q = 1. #Charge of one particle
+    res_x = 1E-4 #Precision on position in x-coordinate
+    name = "mu_100GeV_025T" #label to help differenciate the ouputs
 elif case == 3:
     inf = ROOT.TFile.Open("B5_out_mu_100GeV_1T.root")
     tree = inf.Get("B5")
-    B = 1.
-    delta_x = 1E-4
-    name = "mu_100GeV_1T"
+    B = 1. #Strenght of the magnetic field in Tesla
+    res_x = 1E-4 #Precision on position in x-coordinate
+    q = 1. #Charge of one particle
+    name = "mu_100GeV_1T" #label to help differenciate the ouputs
 elif case == 4:
     inf = ROOT.TFile.Open("B5_out_mu_50GeV_05T.root")
     tree = inf.Get("B5")
-    B = 0.5
-    delta_x = 1E-4
-    name = "mu_50GeV_05T"
+    B = 0.5 #Strenght of the magnetic field in Tesla
+    q = 1. #Charge of one particle
+    res_x = 1E-4 #Precision on position in x-coordinate
+    name = "mu_50GeV_05T" #label to help differenciate the ouputs
 elif case == 5:
     inf = ROOT.TFile.Open("B5_out_mu_200GeV_05T.root")
     tree = inf.Get("B5")
-    B = 0.5
-    delta_x = 1E-4
-    name = "mu_200GeV_05T"
+    B = 0.5 #Strenght of the magnetic field in Tesla
+    q = 1. #Charge of one particle
+    res_x = 1E-4 #Precision on position in x-coordinate
+    name = "mu_200GeV_05T" #label to help differenciate the ouputs
 elif case == 6:
     inf = ROOT.TFile.Open("B5_out_mu_100GeV_05T.root")
     tree = inf.Get("B5")
-    B = 0.5
-    delta_x = 0
-    name = "mu_100GeV_05T_precise"
+    B = 0.5 #Strenght of the magnetic field in Tesla
+    q = 1. #Charge of one particle
+    res_x = 0 #Precision on position in x-coordinate
+    name = "mu_100GeV_05T_precise" #label to help differenciate the ouputs
 elif case == 7:
     inf = ROOT.TFile.Open("B5_out_mu_100GeV_05T_bef_lead.root")
     tree = inf.Get("B5")
-    B = 0.5
-    delta_x = 1E-4
-    name = "mu_100GeV_05T_bef_lead"
+    B = 0.5 #Strenght of the magnetic field in Tesla
+    q = 1. #Charge of one particle
+    res_x = 1E-4 #Precision on position in x-coordinate
+    name = "mu_100GeV_05T_bef_lead" #label to help differenciate the ouputs
 elif case == 8:
     inf = ROOT.TFile.Open("B5_out_mu_100GeV_05T_aft_lead.root")
     tree = inf.Get("B5")
-    B = 0.5
-    delta_x = 1E-4
-    name = "mu_100GeV_05T_aft_lead"
+    B = 0.5 #Strenght of the magnetic field in Tesla
+    q = 1. #Charge of one particle
+    res_x = 1E-4 #Precision on position in x-coordinate
+    name = "mu_100GeV_05T_aft_lead" #label to help differenciate the ouputs
 else : 
     print("input invalide")
 
-p = []
+#Lists#
 
-# Constants #
+momentum = [] #List to stock values of momentum
+
+#Constants#
 L = 2.0 # Length of magnetic field chamber
 dz = 0.5 # Drift chamber wire separation distances
-
+larm_2 = 4.25 # Length of the second arm (taken between the center of the magnetic field chamber and the last wire
 
 def linear(m,x,c): # Straight line function (y=mx+c) to plot reconstructed xz-plane track
     return m*x+c
 
-def gauss(x, a, sigma, mean):
+def gauss(x, a, sigma, mean): #Gaussian function to fit the momentum data
     amplitude = a / ( sigma * np.sqrt(2 * np.pi) )
     part_2 = (x - mean) / sigma
     return amplitude * np.exp( -0.5 * (part_2**2) )
 
-def trackReconstruction(event, chamber): # Reconstruct the xz-plane tracks to output the intercept and gradient of the best fit lline
+def trackReconstruction(event, chamber): # Reconstruct the xz-plane tracks and find the best linear fit for each track
     if chamber == 1:
         xz_pos = [event.Dc1HitsVector_z,event.Dc1HitsVector_x]
     elif chamber == 2:
         xz_pos = [event.Dc2HitsVector_z,event.Dc2HitsVector_x]
-    coordinates = []
+    coordinates = [] #List to stocks the coordinates of the hits in the xz-plane 
     for i in range(len(xz_pos[0])):
-        coordinates.append([xz_pos[0][i]*dz, xz_pos[1][i]/1000])
-    def fit(next):
+        coordinates.append([xz_pos[0][i]*dz, xz_pos[1][i]/1000]) #Adding of the the z position, and then the x position of the hit (/1000 for units)
+    def fit(next): #Chi2 method to find the best linear fit
         nextx = 0
         for z,x in coordinates:
             xp = next[0]*z+next[1]
             nextx += (x-xp)**2 
         return nextx
-    m = (coordinates[-1][1]-coordinates[0][1])/(coordinates[-1][0]-coordinates[0][0])
-    c = coordinates[0][1] 
-    minimum = opt.minimize(fit, x0=[m,c])
-    m = minimum["x"][0]
-    c = minimum["x"][1]
+    m = (coordinates[-1][1]-coordinates[0][1])/(coordinates[-1][0]-coordinates[0][0]) #Compute an initial m value to help the fitting
+    c = coordinates[0][1] #Compute an initial c value to help the fitting
+    minimum = opt.minimize(fit, x0=[m,c]) #Find the best m and c value for the fit by chi2 method
+    m = minimum["x"][0] #Stock the best m value
+    c = minimum["x"][1] #Stock the best c value
 
     return m, c, coordinates
 
-def plotTrack(m1,c1,position1,m2,c2,position2,name):
-    plt.clf()
-    plt.figure()
+def plotTrack(m1,c1,position1,m2,c2,position2,name): #Plot the fitted tracks for one event
+    plt.clf() #Close previous plot canvas
+    plt.figure() #Open a new canvas
     h = plt.gca()
-    z1 = np.linspace(-6.25,0.5,676)
-    z2 = np.linspace(-0.5,4.25,476)
-    x1 = []
-    x2 = []
+    z1 = np.linspace(-6.25,0.5,676) #z values for the tracks in the first arm
+    z2 = np.linspace(-0.5,4.25,476) #z values for the tracks in the secnd arm
+    x1 = [] #List to stock x values corresponding to the first arm
+    x2 = [] #List to stock x values corresponding to the second
     for i in z1 :
-        x1.append(linear(m1,i,c1))
+        x1.append(linear(m1,i,c1)) #Adding the x position corresponding to a linear fit of the tracks in the first arm
     for i in z2 :
-        x2.append(linear(m2,i,c2))
-    plt.plot(z1,x1)
-    plt.plot(z2,x2)
-    h.add_patch(Rectangle((-1,-1), 2, 2, edgecolor='black',facecolor='none'))
+        x2.append(linear(m2,i,c2)) #Adding the x position corresponding to a linear fit of the tracks in the second arm 
+    plt.plot(z1,x1) #Plot the track in the first arm
+    plt.plot(z2,x2) #Plot the track in the second arm
+    h.add_patch(Rectangle((-1,-1), 2, 2, edgecolor='black',facecolor='none')) #Add a square corresponding to the magnetic field chamber
     plt.scatter(position1[0],position1[1])
     plt.scatter(position2[0],position2[1])
-    plt.xlabel("z (m)")
+    plt.xlabel("z (m)") 
     plt.ylabel("x (m)")
     plt.savefig("track_recons_"+name+".pdf")
 #    plt.show()
 
 
 
-def determineMomentum(m1,c1,m2,c2): # Calculates sigma, dx and momentum using Eq.(5) in the notes
+def determineMomentum(m1,c1,m2,c2): # Calculate the momentum (equation explanation in the overleaf)
     sigma = math.atan((m2-m1)/(1+m1*m2))
     dx = abs((m1*(-1)+c1)-(m2*(1)+c2))
-    return (0.3*B*math.sqrt(L**2 + dx**2))/sigma
+    return (0.3*B*math.sqrt(L**2 + dx**2))/(2*math.sin(sigma/2))
 
 
-def plotMomenta(momenta,name): # Plots reconstructed momentum as a histogram and calculates the resolution (width)
-    plt.clf()
+def plotMomenta(momenta,name): #Hist of the momentum
+    plt.clf() #Close previous plot canvas
   
     y, binEdges = np.histogram(momenta, 50)
     x = (binEdges[:-1] + binEdges[1:]) / 2
     x_width = (x[-1] - x[0]) / len(x)
 
-    popt, pcov = opt.curve_fit(gauss, x, y, [300,np.std(momenta), np.mean(momenta)])
+    popt, pcov = opt.curve_fit(gauss, x, y, [300,np.std(momenta), np.mean(momenta)]) #Compute the best amplitude, sigma and mu for a gaussian fit
 
-    plt.bar(x, y, x_width, color="blue", edgecolor="black")
+    plt.bar(x, y, x_width, color="blue", edgecolor="black") #Plot the momentum data
     
     x_int = np.linspace(x[0], x[-1], 10*len(x))
     y_int = gauss(x_int, *popt)
-    plt.plot(x_int, y_int, label="Gaussian fit", color="red") 
+    plt.plot(x_int, y_int, label="Gaussian fit", color="red") #Plot the gaussian fit
     
 
     plt.xticks(fontsize = 12)
@@ -156,20 +166,37 @@ def plotMomenta(momenta,name): # Plots reconstructed momentum as a histogram and
     plt.savefig("Recons_momentum_"+name+".pdf")
     print("[amplitude, sigma, mu]")
     print(popt)
+    print("Error for the same variables")
+    print(pcov)
 
-#def resolution(momenta):
+def resolution(momenta): #Compute the momentum resolution of each event(equation in the overleaf) and do the mean of it
     
-#    return resMomenta
+    ampli = res_x/(0.3*q*B*L*larm_2)
+    resMomenta = [ampli*i for i in momenta]
+    mean_res = sum(resMomenta) 
+    mean_res = mean_res/len(resMomenta)
+    return mean_res
 
 
 
-for event in tree:
-    if len(event.Dc1HitsVector_x)==5 and len(event.Dc2HitsVector_x)==5:
-        m1,c1,position1 = trackReconstruction(event, 1)
-        m2,c2,position2 = trackReconstruction(event, 2)
-        plotTrack(m1,c1,position1,m2,c2,position2,name)
-        recMom = determineMomentum(m1,c1,m2,c2)
-        p.append(abs(recMom))
-plotMomenta(p,name)
-#resMom=resolution(momenta)
+def plotRes(resMomenta,name): # Plot the momentum resolution                      
+    plt.clf() #Close previous plot canvas
+    plt.plot(resMomenta)
+    plt.xlabel("Reconstructed energy [GeV/c]", fontsize = 16)
+    plt.ylabel("Number of events", fontsize = 16)
+    #    plt.show()                                                                                                                           
+    plt.savefig("Recons_resol_"+name+".pdf")
 
+
+
+for event in tree: #Loop over the events
+    if len(event.Dc1HitsVector_x)==5 and len(event.Dc2HitsVector_x)==5: #if there is at least 5 hits in each chamber (possible to do something with less hits in each chamber, however I didn't test it)
+        m1,c1,position1 = trackReconstruction(event, 1) #Linear factors and positions of tracks in the first chamber
+        m2,c2,position2 = trackReconstruction(event, 2) #Linear factors and positions of tracks in the second chamber 
+        computeMom = determineMomentum(m1,c1,m2,c2) #Compute momentum
+        momentum.append(abs(computeMom)) #Stock momentum value
+plotTrack(m1,c1,position1,m2,c2,position2,name) #Plot the fitted tracks
+plotMomenta(momentum,name) #Plot the momentum with a gaussian fit
+resMom=resolution(momentum) #Compute the mean of the momentum resolution
+print(resMom)
+plotRes(resMom,name) #Plot the momentum resolution
--- build/reconstruction_energy.py
+++ build/reconstruction_energy.py
@@ -1,7 +1,7 @@
 import ROOT
 import matplotlib.pyplot as plt
 from matplotlib import rcParams
-import math
+from math import log10, floor
 import scipy.optimize as opt
 import matplotlib.mlab as mlab
 import numpy as np
@@ -11,73 +11,128 @@ from scipy.optimize import curve_fit
 import momentum_track_reconstruction as constr
 
 
-# Arrays and files #
+# Choosing the case to study #
 case = input("chose the case to study by entering : 1 for antimuon, 2 for positron, 3 for proton  \n")
 if case == 1 :
     inf = ROOT.TFile.Open("B5_out_mu_100GeV_05T.root")
     tree = inf.Get("B5")
-    name_file = "mu_100GeV_05T"
-    B = 0.5
-    q = 1.
-    mass = 105.66E-3 #MeV
+    name_file = "mu_100GeV_05T" #Label to differenciate the output
+    B = 0.5 # Stenght of the magnetic field in Tesla
+    q = 1. # Charge of one particle
+    f = 1. # Factor specific to the type of particle
+    mass = 105.66E-3 #Mass of the particle in MeV
 elif case == 2:
     inf = ROOT.TFile.Open("B5_out_e_100GeV_05T.root")
     tree = inf.Get("B5")
-    name_file = "e_100GeV_05T"
-    B = 0.5
-    q = 1.
-    mass = 0.51E-3 #MeV 
+    name_file = "e_100GeV_05T" #Label to differenciate the output
+    B = 0.5 # Stenght of the magnetic field in Tesla 
+    q = 1. # Charge of one particle 
+    f = 1. # Factor specific to the type of particle
+    mass = 0.51E-3 #Mass of the particle in MeV
 elif case == 3 :
     inf = ROOT.TFile.Open("B5_out_p_100GeV_05T.root")
     tree = inf.Get("B5")
-    name_file = "p_100GeV_05T"
-    B = 0.5
-    q = 1.
-    mass = 938.27E-3 #MeV     
+    name_file = "p_100GeV_05T" #Label to differenciate the output
+    B = 0.5 # Stenght of the magnetic field in Tesla 
+    q = 1. # Charge of one particle 
+    f = -1. # Factor specific to the type of particle
+    mass = 938.27E-3 #Mass of the particle in MeV
 else :
     print("not defined case")
 
-ecal_energy = []
-hcal_energy = []
-momentum = []
+ #Lists#
+ecal_energy = [] #List to stock energy from the electromagnetic calorimeter
+hcal_energy = [] #List to stock energy from the hadronic calorimeter
+ecal_energy_corr = [] #List to stock energy from the electromagnetic calorimeter with correction
+hcal_energy_corr = [] #List to stock energy from the hadronic calorimeter with correction
+momentum = [] #List to stock momentum values
+energy_nocorr = [] #List to stock the total energy from both calorimeters
+energy_allcorr = [] #List to stock the total energy from both calorimeters with correction
+
+ #Constants#
+ratio = 20 #ratio of the active to passive layers of the detector
+Z_CsI = 54 #Atomic number for the CsI scintillator (mean between the Cs atomic number and I atomic number)
+X0_CsI = 1.86E-2 #Radiation lenght for CsI scintillator
+E0 = 1.3 #in GeV
+stock = 0 #To stock value to help
+
+
+def round_to(x, y): #Function to round x to the fist non-zero number of y
+    return round(x, -int(floor(log10(abs(y)))))
 
 def plotEnergy(energy,name): # Plots reconstructed energy as a histogram
-    plt.clf()
-    n, bins, patches = plt.hist(energy, bins=50, edgecolor = "black", color = "blue", histtype = 'stepfilled')
+    plt.clf() #Close previous plot canvas
+    
+    y, binEdges = np.histogram(energy, 100)
+    x = (binEdges[:-1] + binEdges[1:]) / 2
+    x_width = (x[-1] - x[0]) / len(x)
+
+#    popt, pcov = opt.curve_fit(constr.gauss, x, y, [100,np.std(energy), np.mean(energy)]) #Find the best amplitude, sigma and mu for a gaussian fit
+    
+    plt.bar(x, y, x_width, color="blue", edgecolor="black") #Plot the energy histo
+
+#    x_int = np.linspace(x[0], x[-1], 10*len(x))
+#    y_int = constr.gauss(x_int, *popt)
+
+#    plt.plot(x_int, y_int, label="Gaussian fit", color="red") #Plot the gaussian fit
     plt.xticks(fontsize = 12)
     plt.yticks(fontsize = 12)
     plt.xlabel("Reconstructed energy [GeV]", fontsize = 16)
     plt.ylabel("Number of events", fontsize = 16)
-    plt.show()
+#    plt.legend()
+#    plt.annotate("sigma (GeV): " + str(round_to(popt[1], pcov[1,1])) + " $\pm$ " + str(round_to(pcov[1,1], pcov[1,1])) + '\n' + "mu (GeV): " + str(round_to(popt[2], pcov[2,2])) + " $\pm$ " + str(round_to(pcov[2,2], pcov[2,2])), xy=(0.025, 0.8), xycoords='axes fraction')
+    #    plt.show() 
     plt.savefig("Reconstructed_energy_"+name+".pdf")
-    return n, bins
+    #print("[amplitude, sigma, mu]")
+    #print(popt)
 
+def EMCal_correction(energy,f): #Compute the correction for the electromagnetic calorimeter (explanation in the overleaf)
+    epsilon = 0.61 / (Z_CsI + 1.24)
+    depth = 0.3 / (X0_CsI)
+    tmax = np.log(energy/1000/epsilon) - f*0.5
+    t95 = tmax + 0.08*54 + 9.6
+    epsilon_corr = (t95/tmax)*epsilon
+    return epsilon_corr * ( 1 - np.exp(t95) ) / ( 1 - np.exp(depth) )
 
-def theorical_energy(momentum,mass):
+def theorical_energy(momentum,mass): #Compute theorical energy for the particle with relativistic relation
     theo_energy = [np.sqrt(i**2 + mass**2) for i in momentum]
     return theo_energy
 
 
-for event in tree: # Main function to run
+for event in tree: # Loop over the events
     if event.ECEnergy != 0:
-        ecal_energy.append(event.ECEnergy)
+        ecal_energy.append(event.ECEnergy/1000) #Add the energy value from the electromagnetic calorimeter (/1000 for units)
+        ecal_energy_for_corr = EMCal_correction(event.ECEnergy,f) #Compute and stock the correction for the electromagnetic calorimeter
+        ecal_energy_corr.append(event.ECEnergy/1000+ecal_energy_for_corr) #Stock the energy with the correction for the electromagnetic calorimeter
     if event.HCEnergy != 0:
-        hcal_energy.append(event.HCEnergy)
-    if len(event.Dc1HitsVector_x)==5 and len(event.Dc2HitsVector_x)==5:
-        m1,c1,position1 = constr.trackReconstruction(event, 1)
-        m2,c2,position2 = constr.trackReconstruction(event, 2)
-        recMom = constr.determineMomentum(m1,c1,m2,c2)
-        momentum.append(abs(recMom))
+        hcal_energy.append(event.HCEnergy/1000) #Add the energy value from the hadronic calorimeter (/1000 for units) 
+        hcal_energy_for_corr = event.HCEnergy/1000 #Stock the value of this event energy from the hadronic calorimeter
+        if hcal_energy_for_corr>E0 : stock = hcal_energy_for_corr*E0 #First correction for the ionisation lost
+        hcal_energy_for_corr=event.HCEnergy/1000*ratio + stock #Second correction for ratio lost applied and adding of the first correction
+        hcal_energy_corr.append(hcal_energy_for_corr) #Stock in a list
+    energy_nocorr.append(event.ECEnergy/1000 + event.HCEnergy/1000) #List for energy from electromagnetic and hadronic calorimeter without any correction
+    energy_allcorr.append((event.ECEnergy)/1000 + hcal_energy_for_corr + ecal_energy_for_corr) #List for energy from electromagnetic and hadronic calorimeter with all corrections                                                           
+
+    if len(event.Dc1HitsVector_x)==5 and len(event.Dc2HitsVector_x)==5: #Only for events with 5 hits in both chambers
+        m1,c1,position1 = constr.trackReconstruction(event, 1) #Compute the best factor for linear fit and position of the hits in the first arm
+        m2,c2,position2 = constr.trackReconstruction(event, 2) #Compute the best factor for linear fit and position of the hits in the second arm
+        computedMom = constr.determineMomentum(m1,c1,m2,c2) #Compute the momentum value of the particle
+        momentum.append(abs(computedMom)) #Stock the value in a list
+
+energy_theo = theorical_energy(momentum,mass) #Compute of the theorical value of energy
 
 
-energy_total = ecal_energy+hcal_energy
-energy_theo = theorical_energy(momentum,mass)
+#Plots of the different energy, from electromagnetic and hadronic calorimeter, with and without the correction
+plotEnergy(ecal_energy,name=name_file+"EMcal_wo_corr")
 
+plotEnergy(hcal_energy,name=name_file+"Hcal_wo_corr")
 
-n, bins = plotEnergy(ecal_energy,name=name_file+"EMcal")
+plotEnergy(energy_nocorr,name=name_file+"total_energy_wo_corr")
 
-n2, bins2 = plotEnergy(hcal_energy,name=name_file+"Hcal")
+#plotEnergy(energy_theo,name=name_file+"total_theo")
 
-n3, bins3 = plotEnergy(energy_nocorr,name=name_file+"total_energy")
+#plotEnergy(energy_allcorr,name=name_file+"total_energy_allcorr")
 
-n4, bins4 = plotEnergy(energy_theo,name=name_file+"total_theo")
+plotEnergy(ecal_energy_corr,name=name_file+"EMcal_corr")
+
+plotEnergy(hcal_energy_corr,name=name_file+"Hcal_corr")
